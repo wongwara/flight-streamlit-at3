2c20d04d248069265f8651bedfab6c3d8143deed
wongwara.w <107904836+wongwara@users.noreply.github.com>
2023-11-06 15:27:03 +1100

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ import pandas as pd
 def main():
     st.title("Data Product with Machine Learning")
 
-@st.cache
 def load_data():
     df = pd.read_csv('https://raw.githubusercontent.com/wongwara/flight-streamlit-at3/main/data/sample_itineraries.csv?token=GHSAT0AAAAAACETSXPAXBU2LOS7R6N4FXIOZKIO7MA')
     X = df[['totalTravelDistance', 'isNonStop', 'isBasicEconomy', 'startingAirport', 'destinationAirport', 'segmentsCabinCode', 'flightDate_day', 'flightDate_month', 'flightDate_year',
