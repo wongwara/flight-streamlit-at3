81fa9b19be9b1f50a837c051e1176beb25f69170
wongwara.w <107904836+wongwara@users.noreply.github.com>
2023-11-06 11:43:42 +1100

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import streamlit as st
 import sys
 import numpy as np
 import pandas as pd 
-import xgboost 
 
 from models.predict_model import print_regressor_scores
 sys.path.insert(0, 'flight-prediction/src/')
