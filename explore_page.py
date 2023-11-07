84040b95c4da10266eb3728fb906c1f22ca70240
wongwara.w <107904836+wongwara@users.noreply.github.com>
2023-11-07 21:28:27 +1100

    Update explore_page.py

--- explore_page.py
+++ explore_page.py
@@ -25,8 +25,8 @@ def show_explore_page():
              Therefore, the objective of this project would be to develop a machine learning model that accepts airport name and flightdate return the predict total fare.
              """
             )
-    #import matplotlib.pyplot as plt
-    #Group data by month and aggregate total fare
+    import matplotlib.pyplot as plt
+    # Group data by month and aggregate total fare
     df['flightDate'] = pd.to_datetime(df['flightDate'])  # Ensure 'flightDate' is in datetime format
     df['Month'] = df['flightDate'].dt.to_period('M')  # Extract the month from 'flightDate'
     grouped_df = df.groupby('Month')['totalFare'].sum().reset_index()
